a0edf3b8c59cd37d30239898ee85e370869e2c54
akulaakula1313-cpu <akulaakula1313@gmail.com>
2026-09-19 17:20:24 +0400

    Change server to listen on all network interfaces

--- server.js
+++ server.js
@@ -727,8 +727,7 @@ if (require.main === module) {
   const { httpServer, socketServer } = createApp();
   setIO(socketServer);
   const PORT = process.env.PORT || 3000;
-  httpServer.listen(PORT, () => console.log(`[Сервер] http://localhost:${PORT}`));
-}
+  httpServer.listen(PORT, '0.0.0.0', () => console.log(`[Сервер] http://localhost:${PORT}`));
 
 module.exports = {
   RANKS, SUITS,
